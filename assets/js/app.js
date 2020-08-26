f645d7275ac119230bcc7d2d077b1703ac0c81fe
popeashiedu <popeashiedu@gmail.com>
2020-08-26 21:21:24 +0100

    section 4 done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,27 @@
-// PRELOADER CODE 
-var overlay = document.getElementById("overlay");
+// // PRELOADER CODE 
+// var overlay = document.getElementById("overlay");
 
-window.addEventListener('load', function () {
-    overlay.style.display = 'none';
-})
+// window.addEventListener('load', function () {
+//     overlay.style.display = 'none';
+// })
 
-// AOS.init({ offset: 300, duration: 800 }); AOS ANIMATION INITIALIZER
-$(function () {
-    AOS.init({ offset: 300, duration: 1000, easing: "ease-out-quad", once: 0 });
-    window.addEventListener('load', AOS.refresh);
-});
+// // AOS.init({ offset: 300, duration: 800 }); AOS ANIMATION INITIALIZER
+// $(function () {
+//     AOS.init({ offset: 300, duration: 1000, easing: "ease-out-quad", once: 0 });
+//     window.addEventListener('load', AOS.refresh);
+// });
 
-// NAVBAR COLLAPSE ON CLICK 
-$(document).on("click", ".navbar-collapse", function (e) {
-    if ($(e.target).is("a")) {
-        $(this).collapse("hide");
-    }
+// // NAVBAR COLLAPSE ON CLICK 
+// $(document).on("click", ".navbar-collapse", function (e) {
+//     if ($(e.target).is("a")) {
+//         $(this).collapse("hide");
+//     }
+// });
+
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
 });
