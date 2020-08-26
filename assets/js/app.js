bf2fee0da66be091dff2ba58ba2719ae4c80accb
popeashiedu <popeashiedu@gmail.com>
2020-08-26 15:38:37 +0100

    ha

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+// // PRELOADER CODE
+// var overlay = document.getElementById("overlay");
+
+// window.addEventListener('load', function () {
+//     overlay.style.display = 'none';
+// })
+
+// // AOS.init({ offset: 300, duration: 800 }); AOS ANIMATION INITIALIZER
+// $(function () {
+//     AOS.init({ offset: 300, duration: 1000, easing: "ease-out-quad", once: 0 });
+//     window.addEventListener('load', AOS.refresh);
+// });
+
+// // NAVBAR COLLAPSE ON CLICK 
+// $(document).on("click", ".navbar-collapse", function (e) {
+//     if ($(e.target).is("a")) {
+//         $(this).collapse("hide");
+//     }
+// });
+
